8bb60cea8149dd7eb75e2b40803f6f019d6fb513
dbarbera <diego@dbarbera.name>
2014-06-17 21:07:46 +0200

    Improve test cases and execute generated code

--- tests/regression/test13/test_ada.c
+++ tests/regression/test13/test_ada.c
@@ -1,6 +1,8 @@
+extern void adainit();
 extern void basic_run();
 
 int main() {
+    adainit();
     basic_run();
     return 0;
 }
---
+++ tests/regression/test13/test_llvm.c
@@ -0,0 +1,8 @@
+extern void basic_startup();
+extern void basic_run();
+
+int main() {
+    basic_startup();
+    basic_run();
+    return 0;
+}
