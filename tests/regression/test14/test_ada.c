b9ece50d8821dba39817a4e6b3f93b9eae4bb4c8
dbarbera <diego@dbarbera.name>
2014-06-26 15:58:26 +0200

    Added test case for sequences

---
+++ tests/regression/test14/test_ada.c
@@ -0,0 +1,8 @@
+extern void adainit();
+extern void sequences_run();
+
+int main() {
+    adainit();
+    sequences_run();
+    return 0;
+}
---
+++ tests/regression/test14/test_llvm.c
@@ -0,0 +1,8 @@
+extern void sequences_startup();
+extern void sequences_run();
+
+int main() {
+    sequences_startup();
+    sequences_run();
+    return 0;
+}
