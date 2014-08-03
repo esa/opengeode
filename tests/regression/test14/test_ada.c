cfd959c189d00743415a2a20f6c461e0516f6d54
dbarbera <diego@dbarbera.name>
2014-08-03 22:24:01 +0200

    Remove test14

--- tests/regression/test14/test_ada.c
+++
@@ -1,8 +0,0 @@
-extern void adainit();
-extern void sequences_run();
-
-int main() {
-    adainit();
-    sequences_run();
-    return 0;
-}
--- tests/regression/test14/test_llvm.c
+++
@@ -1,8 +0,0 @@
-extern void sequences_startup();
-extern void sequences_run();
-
-int main() {
-    sequences_startup();
-    sequences_run();
-    return 0;
-}
