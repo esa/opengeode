7b0b1d0cf7aa7003f87622c0e477b6acb0c3463f
dbarbera <diego@dbarbera.name>
2014-08-04 21:53:39 +0200

    Added test case for control flow constructs

---
+++ tests/regression/test-controlflow/test_llvm.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataview-uniq.h"
+
+extern void controlflow_startup();
+extern void controlflow_run();
+
+void controlflow_RI_assert(asn1SccBoolean *res, asn1SccCharString *msg) {
+    if (!*res) {
+        fprintf(stderr, "%.*s\n", (int)msg->nCount, msg->arr);
+        exit(1);
+    }
+}
+
+void controlflow_RI_fail(asn1SccCharString *msg) {
+    fprintf(stderr, "%.*s\n", (int)msg->nCount, msg->arr);
+    exit(1);
+}
+
+int main() {
+    controlflow_startup();
+    controlflow_run();
+    return 0;
+}
