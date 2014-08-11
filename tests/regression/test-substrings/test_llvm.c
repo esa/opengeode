a99ff7eaf330298510fb5f46a5faef0890da7edc
dbarbera <diego@dbarbera.name>
2014-08-11 12:58:05 +0200

    Fixed test-llvm test files

--- tests/regression/test-substrings/test_llvm.c
+++ tests/regression/test-substrings/test_llvm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dataview-uniq.h"
 
-extern void basic_startup();
+extern void myfunction_startup();
 extern void myfunction_start_something(asn1SccT_Int32 *);
 
 void myfunction_RI_result_data(long long *val)
@@ -15,7 +15,7 @@ int main()
     asn1SccT_Int32 test = 5;
 
     printf("[C Code] Running test\n");
-    basic_startup();
+    myfunction_startup();
     myfunction_start_something(&test);
     return 0;
 }
