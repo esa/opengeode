7ad2e7d1779da906f0f1200c5065980ea46907bd
dbarbera <diego@dbarbera.name>
2014-08-28 14:22:43 +0200

    Refactor test-rules in Makefiles

--- tests/regression/shared.mk
+++ tests/regression/shared.mk
@@ -12,4 +12,23 @@ clean:
 	       real.c xer.c ber.c acn.c asn1crt.c asn1crt.h test_ada test_llvm \
 	       *.autosave
 
+%.o: %.pr FORCE
+	$(OPENGEODE) $< system_structure.pr --llvm
+	$(LLC) $*.ll
+	$(CC) -c $*.s
+
+%.ali: %.pr FORCE
+	$(OPENGEODE) $< system_structure.pr --toAda
+	$(ASN1SCC) -Ada dataview-uniq.asn -typePrefix asn1Scc -equal
+	$(GNATMAKE) -c *.adb
+
+%.o: %.asn FORCE
+	$(ASN1SCC) -c $< -typePrefix asn1Scc -equal
+	$(CC) -c $*.c
+
+%.o: %.c FORCE
+	$(CC) -c $<
+
+FORCE:
+
 .PHONY: clean
