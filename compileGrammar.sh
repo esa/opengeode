0c2ff0b7cbf07c20fc56863cdcefff425d0de0e2
mperroti <maxime.perrotin@esa.int>
2014-10-23 14:32:39 +0200

    Download antlr 3.1.3 when needed to build grammar

--- compileGrammar.sh
+++ compileGrammar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 echo 'you must download and install antlr_python_runtime-3.1.3.tar.gz:
 (from http://pypi.python.org/pypi/antlr_python_runtime/3.1.3 for ex)
@@ -13,6 +13,18 @@ sudo rm $(cat install.record)
 You must also download and unpack antlr-3.1.3.tar.gz
 '
 
-
+if [ ! -d antlr-3.1.3 ]
+then
+    wget http://download.tuxfamily.org/taste/misc/antlr-3.1.3.tar.bz2
+    tar xvf antlr-3.1.3.tar.bz2
+    NO_ANTLR=1
+fi
 export CLASSPATH=$(pwd)/antlr-3.1.3/lib/antlr-3.1.3.jar
 java org.antlr.Tool sdl92.g
+
+if [ $NO_ANTLR ]
+then
+    rm -rf antlr-3.1.3
+    rm antlr-3.1.3.tar.bz2
+fi
+
