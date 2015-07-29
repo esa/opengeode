4a5fafd2f8fc913b230f5763cae7f07fe2c7bcfc
Maxime Perrotin <maxime.perrotin@esa.int>
2015-07-29 10:51:14 +0200

    Update version number

--- opengeode/opengeode.py
+++ opengeode/opengeode.py
@@ -115,7 +115,7 @@ except ImportError:
 
 
 __all__ = ['opengeode', 'SDL_Scene', 'SDL_View', 'parse']
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 if hasattr(sys, 'frozen'):
     # Detect if we are running on Windows (py2exe-generated)
