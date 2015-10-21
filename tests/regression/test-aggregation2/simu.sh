2eef9a749580d49f44401e9b357659d500373b91
Maxime Perrotin <maxime.perrotin@esa.int>
2015-10-21 21:44:03 +0200

    fix dataview and simu.sh script
    dataview contained a type:
    Enum0 ::= ENUMERATED {enum1} which made asn2aadlPlus fail

--- tests/regression/test-aggregation2/simu.sh
+++ tests/regression/test-aggregation2/simu.sh
@@ -6,7 +6,7 @@ asn2aadlPlus dataview-uniq.asn simu/DataView.aadl
 cp libog.so dataview-uniq.asn *.pr simu
 mv *.aadl simu
 cd simu
-aadl2glueC DataView.aadl orchestrator_interface.aadl
+aadl2glueC DataView.aadl og_interface.aadl
 asn2dataModel -toPython dataview-uniq.asn
 make -f Makefile.python
 echo "errCodes=$(taste-asn1-errCodes ./dataview-uniq.h)" >>datamodel.py 
