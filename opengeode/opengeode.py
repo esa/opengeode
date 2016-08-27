aff8e3b0767111c240756d3b9dadd3d73ed64710
Maxime Perrotin <maxime.perrotin@esa.int>
2016-08-27 11:17:14 +0200

    Increase version

--- opengeode/opengeode.py
+++ opengeode/opengeode.py
@@ -137,7 +137,7 @@ except ImportError:
 
 
 __all__ = ['opengeode', 'SDL_Scene', 'SDL_View', 'parse']
-__version__ = '1.5.5'
+__version__ = '1.5.6'
 
 if hasattr(sys, 'frozen'):
     # Detect if we are running on Windows (py2exe-generated)
