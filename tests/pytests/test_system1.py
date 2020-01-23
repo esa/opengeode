cc177e904ef77c5cb52ed24f7751ebc46d1d02c7
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-01-23 13:51:51 +0000

    Add test case for syntax checks

---
+++ tests/pytests/test_system1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from opengeode.ogParser import parser_init, antlr3, sdl92Parser, lexer, check_syntax
+
+# return a string corresponding to a token number:
+token = lambda num: lexer.tokenNamesMap[num]
+
+
+def test_system_with_no_error():
+    ''' Detect the syntax error (missing SEMI after "procedure entry") '''
+    string='''system ss;
+block ss;
+process ss;
+START;
+    NEXTSTATE aa;
+state aa, hh;
+endstate;
+endprocess ss;
+endblock;
+endsystem;
+'''
+    test = parser_init(string=string)
+    # Parse and then check that the reported error is the expected one
+
+    res = test.system_definition()
+    node = res.tree
+    check_syntax(node, node, True, input_string=string)
+
+def test_system_with_error():
+    ''' Detect the syntax error (missing SEMI after "procedure entry") '''
+    string='''system ss;
+block ss;
+process ss;
+START;
+    NEXTSTATE -aa;
+state aa, hh;
+endstate;
+endprocess ss;
+endblock;
+endsystem;
+'''
+    test = parser_init(string=string)
+    # Parse and then check that the reported error is the expected one
+
+    res = test.system_definition()
+    node = res.tree
+    try:
+        check_syntax(node, recursive=True, input_string=string)
+    except SyntaxError:
+        pass
+    else:
+        assert False
+
+if __name__ == '__main__':
+    for name, value in dict(globals()).viewitems():
+        if name.startswith('test_'):
+            print('---- Executing {} ----'.format(name))
+            value()
+            print('---- Done - {} ----\n'.format(name))
