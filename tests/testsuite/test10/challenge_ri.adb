485863419bfbfee72aeebeb56594eccb3bf13de5
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-10-04 08:23:49 +0200

    Delete renamed file

--- tests/testsuite/test10/challenge_ri.adb
+++
@@ -1,8 +0,0 @@
-package body challenge_RI is
-   procedure pow (a : in out asn1SccT_UInt32; b : in out asn1SccT_UInt32; res : out asn1SccT_UInt32) is
-       procedure C_Pow (a : in out asn1SccT_UInt32; b : in out asn1SccT_UInt32; res : out asn1SccT_UInt32) with Import, Convention => C, Link_Name => "challenge_RI_pow";
-   begin
-       C_Pow (a,b, res);
-   end pow;
-
-end challenge_RI;
