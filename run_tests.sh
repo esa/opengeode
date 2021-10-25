308b9cb684d99f9eda030ba6eeb21290d1942105
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-10-25 11:57:22 +0200

    update testscripts

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@ apt update || exit 1
 apt install -y make python3 python3-pip sudo wget || exit 1
 make dependencies || exit 1
 make install || exit 1
-make pytest || exit 1
 make test-ada || exit 1
 exit 0
