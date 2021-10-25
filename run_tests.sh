9c5993f433a4b1f366082f0455cccf660648517e
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-10-25 10:58:44 +0200

    Performing first proper test under Travis CI (inside a Docker container)

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update || exit 1
-apt install -y make || exit 1
+apt install -y make python3 python3-pip sudo || exit 1
 make dependencies || exit 1
 make install || exit 1
 make pytest || exit 1
