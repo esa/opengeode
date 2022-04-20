22aa7a43ede4cd6cf325b4d20d00853161da8266
Maxime Perrotin <maxime.perrotin@esa.int>
2022-04-20 17:09:56 +0200

    Fix references to Pyside2

--- tests/testsuite/TrafficLight_Basic/UserWidgets.py
+++ tests/testsuite/TrafficLight_Basic/UserWidgets.py
@@ -14,9 +14,9 @@ import sys
 import os
 import importlib
 import DV
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 
 from asn1_value_editor import UserWidgetsCommon
 
