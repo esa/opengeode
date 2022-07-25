0de5f31638e0c7f3a15406a04732952fa8f93a84
Maxime Perrotin <maxime.perrotin@esa.int>
2022-07-25 09:58:50 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update || exit 1
-apt install -y make libegl1-mesa python3 python3-pip sudo wget || exit 1
+apt install -y make libegl1-mesa libxkbcommon0 python3 python3-pip sudo wget || exit 1
 make dependencies || exit 1
 make install || exit 1
 make test-ada || exit 1
