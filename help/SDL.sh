c2d89b12be824ed82c5caac4a5301da7949c1f86
Maxime Perrotin <maxime.perrotin@esa.int>
2022-10-17 15:59:59 +0200

    Update help

--- help/SDL.sh
+++ help/SDL.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+git clone https://github.com/maxime-esa/exportMediaWiki2Html
+exportMediaWiki2Html/exportMediaWiki2Html.py --url https://taste.tuxfamily.org/wiki/ --page 357
+exportMediaWiki2Html/exportMediaWiki2Html.py --url https://taste.tuxfamily.org/wiki/ --page 254
 
-./exportMediaWiki2Html.py --url https://taste.tuxfamily.org/wiki/ --page 357
-./exportMediaWiki2Html.py --url https://taste.tuxfamily.org/wiki/ --page 254
-
-
-cd export && ~/opt/spacecreatorenv6/Qt/6.3.1/gcc_64/libexec/qhelpgenerator -o opengeode.qch opengeode.qhp
-cd export && ~/opt/spacecreatorenv6/Qt/6.3.1/gcc_64/libexec/qhelpgenerator -o opengeode.qhc opengeode.qhcp
+cp opengeode.qhp export
+cp opengeode.qhcp export
+cd export && qhelpgenerator -o opengeode.qch opengeode.qhp
+cd export && qhelpgenerator -o opengeode.qhc opengeode.qhcp
 
