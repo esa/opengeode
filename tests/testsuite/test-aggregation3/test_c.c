c4c57fbce0b843e132d5f14e7c1adf63f28ac358
Maxime Perrotin <maxime.perrotin@esa.int>
2023-09-18 13:14:54 +0000

    Cleanup test case

--- tests/testsuite/test-aggregation3/test_c.c
+++ tests/testsuite/test-aggregation3/test_c.c
@@ -1,16 +1,16 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "dataview-uniq.h"
+#include "challenge.h"
+#undef pow
+#include <math.h>
 
-extern void CInitchallenge();
-
-extern void challenge_PI_run();
-extern void challenge_PI_any_one();
-extern void challenge_PI_any_two();
-
-void challenge_RI_pow(long long *a, long long *b, long long *res)
+void challenge_RI_pow(asn1SccT_UInt32 * a,asn1SccT_UInt32 * b,asn1SccT_UInt32 * res)
 {
-    *res = (long long)pow((double)*a, (double)*b);
+    double da, db;
+    da = (double) *a;
+    db = (double) *b;
+    *res = (long long)pow(da, db);
 }
 
 void challenge_check_queue(bool *res)
@@ -22,7 +22,7 @@ int main()
 {
     printf("Testing parallel states...\n");
 
-    CInitchallenge();
+    challenge_startup();
 
     challenge_PI_run();
     challenge_PI_run(); // should have no effect
