195f03e91ee3fdc5caa3f53639876a6b2595ab5b
Maxime Perrotin <maxime.perrotin@esa.int>
2023-09-25 08:47:59 +0000

    Update test case

--- tests/testsuite/test-controlflow/test_c.c
+++ tests/testsuite/test-controlflow/test_c.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dataview-uniq.h"
+#include "controlflow.h"
 
-extern void run();
+extern void controlflow_startup();
+extern void controlflow_PI_run();
 
 void controlflow_RI_assert(asn1SccBoolean *res, asn1SccCharString *msg) {
     if (!*res) {
@@ -17,7 +19,8 @@ void controlflow_RI_fail(asn1SccCharString *msg) {
 }
 
 int main() {
-    run();
+    controlflow_startup();
+    controlflow_PI_run();
     return 0;
 }
 
