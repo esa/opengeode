0270f1ee5657c6ec296d58aefd36200bdca4c8b6
Maxime Perrotin <Maxime.Perrotin@esa.int>
2023-10-12 11:34:43 +0200

    Add rtd

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Opengeode'
+copyright = '2012-2023, Maxime Perrotin / European Space Agency'
+author = 'Maxime Perrotin'
+
+release = '1.0'
+version = '4.0.6'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Welcome to Opengeode's documentation!
+===================================
+
+**OpenGEODE** is a free and open source SDL editor
+
+Check out the :doc:`usage` section for further information, including
+how to :ref:`installation` the project.
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   usage
+   api
