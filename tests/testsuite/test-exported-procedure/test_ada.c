e770e0d7cb287d940619f09f8d544d33fecaf3a3
Maxime Perrotin <Maxime.Perrotin@esa.int>
2026-06-21 22:14:36 +0200

    Update test case

--- tests/testsuite/test-exported-procedure/test_ada.c
+++ tests/testsuite/test-exported-procedure/test_ada.c
@@ -2,13 +2,14 @@
 #include "dataview-uniq.h"
 
 extern void adainit();
-extern void og_PI_hehe(asn1SccType2 *p);
 
+extern void og_PI_hehe(asn1SccToto *t, asn1SccType2 *p);
 int main() {
     asn1SccType2 p = false;
+    asn1SccToto t = { 42, true };
     printf("%s\n", !p?"OK":"ERROR");
     adainit();
-    og_PI_hehe(&p);
+   og_PI_hehe(&t, &p);
     printf("%s\n", p?"OK":"ERROR");
     return 0;
 }
