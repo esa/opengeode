3b01d475f99272f7b44a8259f90d32df28372538
Maxime Perrotin <Maxime.Perrotin@esa.int>
2026-06-28 11:58:14 +0200

    Update typo in help generator

--- help/SDL.sh
+++ help/SDL.sh
@@ -10,8 +10,8 @@ fi
 
 if [ $? -eq 0 ]; then
     ./process_gitlab_wiki.py && \
-    mv html_output/opengeode.qch .. && \
-    mv html_output/opengeode.qhc ..
+    mv html_output/opengeode.qch . && \
+    mv html_output/opengeode.qhc .
 else
     echo "Failed to retrieve or update wiki."
     exit 1
