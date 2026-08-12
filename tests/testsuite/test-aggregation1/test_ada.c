0768c984e7a6f107977a947c0bfae29210b811bb
Maxime Perrotin <Maxime.Perrotin@esa.int>
2026-08-12 14:39:28 +0200

    Update  test case

--- tests/testsuite/test-aggregation1/test_ada.c
+++ tests/testsuite/test-aggregation1/test_ada.c
@@ -4,9 +4,8 @@
 /* Ada code external interface */
 extern void challenge_PI_run();
 extern void challenge_PI_any_one();
+extern void challenge_PI_any_two();
 extern int adainit();
-
-//extern long long l_result;
 
 /* Provide code called by the Ada state machine as external procedure */
 void challenge_RI_pow(long long *a, long long *b, long long *res)
@@ -18,11 +17,12 @@ int main()
 {
     adainit();
     challenge_PI_run();
+    challenge_PI_any_two();
+    challenge_PI_any_one();
     challenge_PI_run();
-    //printf("Internal state: %lld\n", l_result);
+    challenge_PI_any_two();
     challenge_PI_any_one();
     challenge_PI_run();
     
     return 0;
 }
-
